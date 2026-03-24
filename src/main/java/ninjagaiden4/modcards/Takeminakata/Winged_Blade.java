677ea16480d1b88880fc3a3010e50747bd04920a
lingqiu1700 <2917074067@qq.com>
2026-03-25 02:25:17 +0800

    0.0.1.4

--- src/main/java/ninjagaiden4/modcards/Takeminakata/Winged_Blade.java
+++ src/main/java/ninjagaiden4/modcards/Takeminakata/Winged_Blade.java
@@ -1,14 +1,12 @@
 package ninjagaiden4.modcards.Takeminakata;
 
 import basemod.abstracts.CustomCard;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import ninjagaiden4.actions.core;
-import ninjagaiden4.helpers.ModHelper;
 import ninjagaiden4.helpers.WeaponCard;
 import ninjagaiden4.modcore.WeaponTags;
 
